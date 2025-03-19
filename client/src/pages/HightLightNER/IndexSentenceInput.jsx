2ba0ef9ee592e04fb9c1030d0424d35438e97f08
vanthan04 <thanvantran1707@gmail.com>
2025-03-20 01:11:55 +0700

    update interface v2

--- client/src/pages/HightLightNER/IndexSentenceInput.jsx
+++ client/src/pages/HightLightNER/IndexSentenceInput.jsx
@@ -1,23 +1,41 @@
-const IndexSentenceInput = ({ indexSentence, setIndexSentence }) => {
+import { useState } from "react";
+
+const IndexSentenceInput = ({setText, setIndexSentence }) => {
+   const [inputText, setInputText] = useState("");
+
+   const handleOnClickButton = () => {
+      if (!isNaN(inputText) && Number.isInteger(Number(inputText))) {
+         setIndexSentence(inputText);
+         setText("");
+      } else {
+         setText(inputText);
+         setIndexSentence("");
+      }
+      setIndexSentence(inputText);
+   };
+
+   const handleOnChange = (e) => {
+      setInputText(e.target.value);
+   };
+
    return (
      <div style={{ display: "flex", alignItems: "center", marginBottom: "10px" }}>
        <label style={{ marginRight: "10px", whiteSpace: "nowrap" }}>
-         Nhập số thứ tự của câu:
+         Nhập câu hoặc số thứ tự của câu:
        </label>
        <input
-         value={indexSentence}
-         onChange={(e) => setIndexSentence(e.target.value)}
-         style={{
-           width: "100%", 
-           padding: "10px",
-           fontSize: "16px",
-           flex: 1,
-         }}
+         value={inputText}
+         onChange={handleOnChange}
+         style={{ width: "100%", padding: "10px", fontSize: "16px", flex: 1 }}
        />
-       {/* <button style={{marginLeft: "20px", padding: "15px"}}>Xác nhận</button> */}
+       <button
+         onClick={handleOnClickButton}
+         style={{ marginLeft: "20px", padding: "15px" }}
+       >
+         Xác nhận
+       </button>
      </div>
    );
- };
- 
- export default IndexSentenceInput;
- 
+};
+
+export default IndexSentenceInput;
