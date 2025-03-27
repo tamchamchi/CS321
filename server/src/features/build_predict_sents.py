86f2fd09ab2a081a6de2c3aa0da35375646b3549
Tam Anh Nguyen Duy <nguyenduytamanh03012004@gmail.com>
2025-03-27 13:45:29 +0700

    Complete tools

--- server/src/features/build_predict_sents.py
+++ server/src/features/build_predict_sents.py
@@ -11,6 +11,7 @@ def build_predict_sents():
     # Load data an toàn với quotechar
     dataset = pd.read_csv(PROCESSED_DATA_DIR / "clear_label_id.csv")
     count = 0
+    flag = 0
     
     result = []
     for idx, sent in tqdm(enumerate(dataset["sentences"]), desc="Predicting...", total=len(dataset)):
@@ -21,12 +22,15 @@ def build_predict_sents():
         tags = convert_to_ner_format(tokens, labels)  # Chuyển đổi định dạng
 
         if len(tags.get("tags")) > 0:
+            flag = 1
             count += 1
 
         sent_format_xml = insert_tags(tags)  # Gắn thẻ vào câu
         tags = json.dumps(tags, ensure_ascii=False, indent=2)
-
-        result.append([idx, sent, sent_format_xml, tags])  # Lưu kết quả
+        
+        if flag == 1:
+            result.append([idx, sent, sent_format_xml, tags])  # Lưu kết quả
+            flag = 0    
 
     # Save result với quoting an toàn
     with open(PROCESSED_DATA_DIR / "predict_result.csv", "w", encoding="utf-8", newline='') as file:
